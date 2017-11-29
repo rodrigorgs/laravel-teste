6fafe278eb49447db2a316890f0ffddecdcc1871
rodrigorgs <rodrigorgs@gmail.com>
2017-11-28 19:55:00 -0500

    Adiciona exemplo de templates.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index() {
-      return view('home.index');
+      return view('home.index',
+                  ['nome' => 'Fulano',
+                   'itens' => ['laranja', 'maçã', 'pera']]);;
     }
 }
