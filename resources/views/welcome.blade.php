8753976c2c02441b0ba3dc934135a4bb350c129a
Naara <naara.musse@gmail.com>
2018-02-01 21:23:30 -0300

    link para acesso ao sistema na pagina inicial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,14 @@
 @extends('home.app')
 @section('main')
-<div>
-  {{--Linha quebrando o master --}}
-  {{--<p> <a href="/login">{!! app(App\Http\Controllers\PhpCasAuthenticationController::class)->infophpcas() !!}</a></p> --}} 
-</div>
+    <div>
+        {{--Linha quebrando o master --}}
+        {{--<p> <a href="/login">{!! app(App\Http\Controllers\PhpCasAuthenticationController::class)->infophpcas() !!}</a></p> --}}
+        <p><a href="{{url('admin')}}">Acesso ao sistema</a></p>
+    </div>
     <div class="title m-b-md">
         Testando o deploy!!!
     </div>
-<div>
-  <p><a href="/mail/enviar">Enviar email</a></p>
-</div>
+    <div>
+        <p><a href="/mail/enviar">Enviar email</a></p>
+    </div>
 @endsection
